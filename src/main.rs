d77fa952e161afcd6bce882805b03d61d921baab
Vladimir <8599007+vladimirdotk@users.noreply.github.com>
2024-04-03 11:43:12 +0300

    Add command init and args parser. (#2)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,58 @@
+use clap::{Arg, ArgAction, ArgMatches, Command};
+
+enum CountMode {
+    Bytes,
+    Lines,
+    Words,
+    All,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = args();
+    let _ = count_mode(&args);
+    let _ = args.get_one::<String>("file").unwrap();
+}
+
+fn args() -> ArgMatches {
+    Command::new("wc-clome")
+        .version("0.1.0")
+        .author("Vladimir K")
+        .about("Count bytes, lines, words")
+        .arg(
+            Arg::new("bytes")
+                .short('c')
+                .action(ArgAction::SetTrue)
+                .help("Count bytes"),
+        )
+        .arg(
+            Arg::new("lines")
+                .short('l')
+                .action(ArgAction::SetTrue)
+                .help("Count lines"),
+        )
+        .arg(
+            Arg::new("words")
+                .short('w')
+                .action(ArgAction::SetTrue)
+                .help("Count words"),
+        )
+        .arg(
+            Arg::new("file")
+                .help("File to process")
+                .required(true)
+                .index(1),
+        )
+        .get_matches()
+}
+
+fn count_mode(args: &ArgMatches) -> CountMode {
+    if args.get_flag("bytes") {
+        CountMode::Bytes
+    } else if args.get_flag("lines") {
+        CountMode::Lines
+    } else if args.get_flag("words") {
+        CountMode::Words
+    } else {
+        CountMode::All
+    }
 }
